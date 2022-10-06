a85d7a305754e299f58e2fc03cef90818192575a
Hawaii-98 <112494005+Hawaii-98@users.noreply.github.com>
2022-10-06 20:17:23 +0800

    Update 8.1Search code.c

--- Data Struct/Search/8.1Search code.c
+++ Data Struct/Search/8.1Search code.c
@@ -86,6 +86,55 @@ int Interpolation_Search(int *a, int n, int key)
     }
 }
 
+//斐波那契查找
+int Fibonacci_Search(int *a, int n, int key)
+{
+    //建立斐波那契数列
+    int F[100];
+    F[0] = 0;
+    F[1] = 1;
+    int i;
+    for (i = 2; i < 100; i++)
+    {
+        F[i] = F[i - 1] + F[i - 2];
+    }
+
+    //斐波那契查找
+    int low, high, mid, k = 0;
+    low = 1;             /* 定义最低下标为记录首位 */
+    high = n;            /* 定义最高下标为记录末位 */
+    while (n > F[k] - 1) //计算n位斐波那契数列的位置
+        k++;
+    for (i = n; i < F[k] - 1; i++) //将原数组中不满的数据补全，以便使用斐波那契查找；
+        a[i] = a[n];
+    while (low <= high)
+    {
+        mid = low + F[k - 1] - 1;
+        if (key < a[mid])
+        {
+            high = mid - 1;
+            k = k - 1;
+        }
+        else if (key > a[mid])
+        {
+            low = mid + 1;
+            k = k - 2;
+        }
+        else
+        {
+            if (mid <= n)
+            {
+                return mid;
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+    return 0;
+}
+
 /****************主函数*************************/
 int main()
 {
@@ -102,4 +151,7 @@ int main()
 
     result = Interpolation_Search(arr, 10, 62);
     printf("Interpolation_Search:%d \n", result);
+
+    result = Fibonacci_Search(arr, 10, 62);
+    printf("Fibonacci_Search:%d \n", result);
 }
